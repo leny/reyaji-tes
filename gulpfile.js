aefb12c28764871b440a2430f3734b34e7b2bcac
leny <info@flatland.be>
2016-02-12 00:02:41 +0100

    Setup env & tasks

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+/* Reyaji/tès
+ *
+ * /gulpfile.js - tasks
+ *
+ * coded by leny@flatLand!
+ * started at 12/02/2016
+ */
+
+/* eslint-disable */
+
+"use strict";
+
+var gulp = require( "gulp" ),
+    gbabel = require( "gulp-babel" ),
+    browserify = require( "browserify" ),
+    babelify = require( "babelify" ),
+    source = require( "vinyl-source-stream" );
+
+// react tasks
+
+gulp.task( "react", function() {
+    return browserify( "./src/main.js" )
+        .transform( babelify, {
+            "presets": [ "es2015", "react" ]
+        } )
+        .bundle()
+        .pipe( source( "main.js" ) )
+        .pipe( gulp.dest( "./js" ) );
+} )
+
+// default task
+
+gulp.task( "default", [ "react" ] );
